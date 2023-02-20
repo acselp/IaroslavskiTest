7bd9b9782cc79e19b0c9a32358dce6cd17b0b9e7
verj <virgiliu.plesca@iis.utm.md>
2023-02-21 00:35:05 +0200

    footer modified

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,48 @@
 import "./Footer.scss";
+import FooterImg from "../../img/footer-logo.png";
+import XingLogo from "../../img/xing-logo.png";
+import LinkedinLogo from "../../img/linkedin-logo.png";
+import qr from "../../img/QR.png";
 
+ 
 function Footer() {
 
     return (
         <div className="footer aos-init aos-animate" data-aos="fade" data-aos-duration="500">
+            <div className="footer-img">
+
+                <img src={FooterImg} alt="" />
+                </div>
+
+                <div>
+                    <img src={qr} />
+                </div>
+
+                <div>
+
+                <p className="connect">
+                    <b>
+                        Connect
+                    </b>
+                </p>
+                <div className="footer-social">
+                    <div className="xing">
+                        <a href="https://www.xing.com/companies/b&blogistics">
+                            <img src={XingLogo} alt="" />
+                        </a>
+                    </div>
+                    <div className="linkedin">
+                        <a href="https://www.linkedin.com/company/b&b-logistic">
+                            <img src={LinkedinLogo} alt="" />
+                        </a>
+                    </div>
+                </div>
+                <p>
+                    <b>
+                        We create opportunities
+                    </b>
+                </p>
+            </div>
             <div className="footer-right">
                 <b>B&B LOGISTICS, SRL<br/> </b>str. Zelinski 7, of. 210 <br/> CHISINAU <br/>MD-2015 R. of Moldova 
             </div>
